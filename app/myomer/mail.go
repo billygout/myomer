22aafef5e80966d2551cd01f9e860ff9f49e8da2
Corey Kasten <coreykasten@myomer.net>
2012-05-03 20:12:37 -0400

    complete parsing of mail body

--- app/myomer/mail.go
+++ app/myomer/mail.go
@@ -7,7 +7,12 @@ import (
 	appmail "appengine/mail"
 	"net/mail"
 	"errors"
+	"fmt"
+	"bufio"
+	"strings"
 )
+
+var mail_seqnum = int64(0)
 
 type InfoWriter struct {
 	appengine.Context
@@ -28,12 +33,87 @@ func addressListToStringSlice(list []*mail.Address) []string {
 	return sl
 }
 
+func parseBody(body io.Reader) (plain, html string, err error) {
+	var str string
+	var delim string = ""
+	var in_plain bool = false
+	var in_html bool = false
+	var need_skip bool = false
+	var is_complex = false
+	var simple_text string = ""
+	
+	var line []byte
+	var isPrefix bool
+
+	plain = ""
+	html = ""
+	err = nil
+
+	bufr := bufio.NewReader(body)
+	for {
+		if line, isPrefix, err = bufr.ReadLine(); err != nil || isPrefix  {
+			if err != nil && err != io.EOF {
+				return "", "", errors.New(fmt.Sprintf("error parsing body: %v", err))
+			} else if isPrefix {
+				return "", "", errors.New("error parsing body: parsed incomplete line")
+			}
+			
+			break
+		} else {
+			str = (string(line))
+			simple_text += str + "\n"
+			switch {
+			case delim == "":
+				delim = str
+			case strings.Contains(str, delim):
+				in_plain = false
+				in_html = false
+			case strings.HasPrefix(str, "Content-Type:"):
+				is_complex = true
+				if strings.Contains(str, "text/plain") {
+					in_plain = true
+				} else if strings.Contains(str, "text/html") {
+					in_html = true
+				}
+				need_skip = true
+			case need_skip:
+				need_skip = false
+			default:
+				if in_plain {
+					plain += str + "\n"
+				} else if in_html {
+					html += str + "\n"
+				}
+			}
+			
+		}
+	}
+
+	if !is_complex {
+		plain = simple_text
+		html = ""
+	} else {
+		plain = strings.TrimSpace(plain)
+		if plain[0] == '*' {
+			plain = plain[1:]
+		}
+		if plain[len(plain)-1] == '*' {
+			plain = plain[0:len(plain)-1]
+		}
+	}
+	
+	err = nil
+	return
+}
+
 func parseMessage(msg *mail.Message) (*appmail.Message, error) {
 
 	var from string
 	var to []*mail.Address
 	var cc []*mail.Address
 	var bcc []*mail.Address
+	var body, html string
+	var err error
 
 	// required: "From"
 	if from = msg.Header.Get("From"); from == "" {
@@ -55,6 +135,11 @@ func parseMessage(msg *mail.Message) (*appmail.Message, error) {
 	replyto := msg.Header.Get("ReplyTo")
 	subject := msg.Header.Get("Subject")
 	
+	// parse body
+	if body, html, err = parseBody(msg.Body); err != nil {
+		return nil, err
+	} 
+	
 	return &appmail.Message{
 	Sender: from,
 	ReplyTo: replyto,
@@ -62,6 +147,8 @@ func parseMessage(msg *mail.Message) (*appmail.Message, error) {
 	Cc: addressListToStringSlice(cc),
 	Bcc: addressListToStringSlice(bcc),
 	Subject: subject,
+	Body: body,
+	HTMLBody: html,
 	}, nil
 }
 
@@ -89,23 +176,27 @@ func incomingMail(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if appmsg, err = parseMessage(msg); err != nil {
-		c.Infof("error parsing message: %v", err)
+	/*
+	iw := InfoWriter{c}
+
+	c.Infof("BEGIN BODY...")
+	io.Copy(iw, msg.Body)
+	c.Infof("...END BODY")
+	 */
+	
+	 if appmsg, err = parseMessage(msg); err != nil {
+		c.Infof("%v", err)
 		return
 	} 
-
+	
 	c.Infof("Received mail: %+v", appmsg)
-
 	
-	iw := InfoWriter{c}
+	body := fmt.Sprintf("You said: \"%s\"\n(seqnum: %v)\n", appmsg.Body, mail_seqnum)
 	
-	if _, err = io.Copy(iw, msg.Body); err != nil {
-		c.Infof("Error: %v", err)
-	} 
-        
-
 	// sent reply
-	if err = sendMail(c, appmsg.To[0], appmsg.Sender, "Automatic Reply..."); err != nil {
+	if err = sendMail(c, appmsg.To[0], appmsg.Sender, body); err != nil {
 		c.Infof("Error sending reply: %v", err) 
 	}
+        
+	mail_seqnum++
 }
